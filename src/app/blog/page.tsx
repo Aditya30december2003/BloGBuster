f36019d606a976b9f4a4fe44f5203f41a9c703fb
Aditya30december2003 <adityasmjain@gmail.com>
2024-07-11 01:05:31 +0530

    changes in BlogProps

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -2,21 +2,36 @@ import CardList from '@/Components/CardList/CardList'
 import Menu from '@/Components/Menu/Menu'
 import React from 'react'
 
-const blog = ({searchParams}) => {
-  const page = parseInt(searchParams)||1;
-  const {cat} = searchParams;
-  function Capitalize(string){
-    return string.charAt(0).toUpperCase() + string.slice(1)
-  }
-  return (
-    <div>
-    <div className='bg-orange-500 text-white text-[1.2rem] font-bold text-center p-2'>{cat ?  Capitalize(cat)  : <></> }blog</div>
-    <div className="flex flex-col lg:flex-row  gap-[50px] mt-[1.2rem] w-full">
-    <div className="w-[100%] lg:w-[75%]"><CardList page={page} cat={cat}/></div>
-    <div className="w-[100%] lg:w-[25%]"><Menu /></div>
-    </div>
-    </div>
-  )
+interface BlogProps {
+  searchParams: {
+    page?: number;
+    cat?: string;
+  };
 }
 
-export default blog
+const Blog: React.FC<BlogProps> = ({ searchParams }) => {
+  const { page = 1, cat } = searchParams || {};
+  
+  function capitalize(string: string | undefined) {
+    return string ? string.charAt(0).toUpperCase() + string.slice(1) : '';
+  }
+
+  return (
+    <div>
+      <div className='bg-orange-500 text-white text-[1.2rem] font-bold text-center p-2'>
+        {cat ? capitalize(cat) + ' Blog' : 'Blog'}
+      </div>
+      <div className="flex flex-col lg:flex-row gap-[50px] mt-[1.2rem] w-full">
+        <div className="w-full lg:w-[75%]">
+          <CardList page={page} cat={cat} />
+        </div>
+        <div className="w-full lg:w-[25%]">
+          <Menu />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Blog;
+
